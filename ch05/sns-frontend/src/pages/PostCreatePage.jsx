e34bca3c439c3df49c11f479665671560653f2f8
ggumugi <phs970425@gmail.com>
2024-12-18 10:52:53 +0900

    navigate-issue

--- ch05/sns-frontend/src/pages/PostCreatePage.jsx
+++ ch05/sns-frontend/src/pages/PostCreatePage.jsx
@@ -14,7 +14,8 @@ const PostCreatePage = () => {
          dispatch(createPostThunk(postData))
             .unwrap()
             .then(() => {
-               navigate('/') // 등록 후 메인 페이지 이동
+               // navigate('/') // 등록 후 메인 페이지 이동
+               window.location.href = '/'
             })
             .catch((err) => {
                console.error('게시물 등록 실패 : ', err)
